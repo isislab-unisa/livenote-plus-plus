e6b4afd661c5c44275f1c2f8e9b0ece06daef048
ddevin96 <danieledevinco1996@gmail.com>
2020-09-28 11:30:59 +0000

    :rocket: preAlpha

--- public/master/livenote-master.js
+++ public/master/livenote-master.js
@@ -351,11 +351,6 @@ module.exports = {
         option => option.text === stream.getAudioTracks()[0].label
       );
       videoElement.srcObject = stream;
-      /*const tracks = stream.getTracks();
-      tracks.forEach(function(track) {
-        track.stop();
-        console.log('stopping')
-      })*/
       socket.emit("broadcaster");
     }
       
